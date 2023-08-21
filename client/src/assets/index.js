6074b974122a247c4451dcca9449fdb5509d2ef0
Grenish rai <ariestheracer@gmail.com>
2023-08-21 17:44:12 +0530

    import/export

---
+++ client/src/assets/index.js
@@ -0,0 +1,15 @@
+import dipendra from "./dipendra.jpeg";
+import gaurav from "./gaurav.jpeg";
+import parimala from "./parimala.jpeg";
+import feedbackFav from "./feedback-favicon.svg";
+import feedbackLogo from "./feedback-logo.svg";
+import faculty from "./faculty.png";
+
+export {
+    dipendra,
+    gaurav,
+    parimala,
+    feedbackFav,
+    feedbackLogo,
+    faculty
+}
