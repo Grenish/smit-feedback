41c905d7173b897af28d4ca856329894ae6c44de
Grenish rai <ariestheracer@gmail.com>
2023-08-22 21:15:25 +0530

    Update Newsfeed.jsx

--- client/src/components/Newsfeed.jsx
+++ client/src/components/Newsfeed.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Newsfeed = () => {
   return (
-    <div className='w-full'>
-      <div className="w-1/2 h-52 m-auto bg-timberWolf"></div>
-    </div>
+    <>
+    
+    </>
   )
 }
 
